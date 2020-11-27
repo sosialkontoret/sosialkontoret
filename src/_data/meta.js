cee203357778a693b9f26ff02ee81e27156b6b20
Sindre Bøyum <sindre.boyum@gmail.com>
2020-11-27 13:36:26 +0100

    Update site description

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,7 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
   siteName: "Sosialkontoret",
-  siteDescription:
-    "",
+  siteDescription: "Welcome to Sosialkontoret! We'll help you cope with your colleagues in trying times ☀️",
   authorName: "Sosialkontoret",
   twitterUsername: "sindreboyum", // no `@`
 };
