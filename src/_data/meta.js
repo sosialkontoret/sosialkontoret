fd3e147ffd83d6f95f2d9bf6f35507195c206fe9
Sindre Bøyum <sindre.boyum@gmail.com>
2020-11-27 09:50:51 +0100

    Update meta

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "Sosialkontoret",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed, and social share preview images.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "",
+  authorName: "Sosialkontoret",
+  twitterUsername: "sindreboyum", // no `@`
 };
